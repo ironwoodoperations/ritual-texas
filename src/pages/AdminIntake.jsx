9ee393357c4f904198d3c81e8677e52f1dd6f44a
scott devore <scottdevore2@gmail.com>
2026-03-19 13:50:08 +0000

    File changes

--- src/pages/AdminIntake.jsx
+++ src/pages/AdminIntake.jsx
@@ -969,6 +969,7 @@ export default function AdminIntake() {
           </div>
         )}
 
+        <PageHelpBanner title="Reservation Intake" content={INTAKE_HELP} accentColor="rgb(107,85,64)" />
         <div className="flex flex-col sm:flex-row gap-3">
           <div className="relative flex-1">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-[rgb(150,150,150)]" />
